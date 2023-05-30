a2fb32482a5d96500a9f53752235b23952654b31
dmaheshwari2209 <100116359+dmaheshwari2209@users.noreply.github.com>
2023-05-30 20:25:33 +0530

    custom data struct transfer over network

--- Assets/PlayerNetwork.cs
+++ Assets/PlayerNetwork.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using UnityEngine;
 using Unity.Netcode;
 
@@ -7,9 +8,27 @@ public class PlayerNetwork : NetworkBehaviour
 {
     private NetworkVariable<int> randomNumber = new NetworkVariable<int>(1, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
 
+    private NetworkVariable<CustomData> customData = new NetworkVariable<CustomData>(new CustomData{_int = 1, _bool = false, message = "Starting"}, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Owner);
+
+    public struct CustomData : INetworkSerializable {
+        public int _int;
+        public bool _bool;
+        public FixedString128Bytes message;
+
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter {
+            serializer.SerializeValue(ref _int);
+            serializer.SerializeValue(ref _bool);
+            serializer.SerializeValue(ref message);
+        }
+    }
+
     public override void OnNetworkSpawn() {
         randomNumber.OnValueChanged += (int previousValue, int newValue) => {
             Debug.Log(OwnerClientId + "; randomNumber: " + randomNumber.Value);
+        };
+
+        customData.OnValueChanged += (CustomData previousValue, CustomData newValue) => {
+            Debug.Log(OwnerClientId + "; " + customData.Value._int + "; " + customData.Value._bool + "; " + customData.Value.message);
         };
     }
 
@@ -19,6 +38,7 @@ public class PlayerNetwork : NetworkBehaviour
 
         if(Input.GetKeyDown(KeyCode.T)){
             randomNumber.Value = Random.Range(0, 100);
+            customData.Value = new CustomData {_int = Random.Range(0, 10), _bool = true, message = "Randomizing"};
         }
 
         Vector3 moveDir = new Vector3(0,0,0);
